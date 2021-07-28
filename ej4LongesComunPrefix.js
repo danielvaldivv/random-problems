de8106b36ec5d043577e6e9dfd370e0d3812f6ce
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-07-27 22:34:31 -0500

    Ej4

---
+++ ej4LongesComunPrefix.js
@@ -0,0 +1,49 @@
+var longestCommonPrefix = function(strs) {
+    let array1 = []
+    let array2 = []
+    let array3 = []
+
+    for (i=0; i < strs.length; i++){
+        if(i===0) {
+            let push = strs[0]
+            array1.push(push)
+        } else if(i===1) {
+            let push = strs[1]
+            array2.push(push)
+        } else {
+            let push = strs[2]
+            array3.push(push)
+        }
+    }
+    console.group("Arrays")
+    console.log(array1[0])
+    console.log(array2[0])
+    console.log(array3[0])
+    console.groupEnd()
+
+    let text1 = array1[0]
+    let text2 = array2[0]
+    let text3 = array3[0]
+
+
+    let array1Separate = text1.split("", text1.length)
+    let array2Separate = text2.split("", text2.length)
+    let array3Separate = text3.split("", text3.length)
+
+    console.group("Words")
+    console.log(array1Separate)
+    console.log(array2Separate)
+    console.log(array3Separate)
+    console.groupEnd()
+
+    let arraySolution = []
+
+    for (i=0; i<text1.length; i++){
+        if (array1Separate[i] == array2Separate[i] && array2Separate[i]== array3Separate[i]) {
+            arraySolution.push(array1Separate[i])
+        }
+    }
+    console.log(arraySolution)
+};
+
+longestCommonPrefix(["Dario", "Daniel", "David"])
