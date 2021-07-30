f4563aac791b6efd3690348ef2c1ca352b831321
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-07-29 21:35:20 -0500

    ej5 solved

---
+++ ej5ValidParentheses.js
@@ -0,0 +1,33 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+
+var isValid = function(s) {
+    let arraySeparate = s.split('', s.length);
+    console.log(arraySeparate);
+
+    for (j=0; j < arraySeparate.length - 1 ; j++) {
+        if (arraySeparate[j] == '[' && arraySeparate[j+1]==']') {
+            arraySeparate.splice(j,2)
+            j=-1
+        } else if (arraySeparate[j] == '{' && arraySeparate[j+1]=='}') {
+            arraySeparate.splice(j,2)
+            j=-1
+        } else if (arraySeparate[j] == '(' && arraySeparate[j+1]==')') {
+            arraySeparate.splice(j,2)
+            j=-1
+        }
+    }
+
+    if (arraySeparate.length == 0){
+        console.log('true')
+    } else {
+        console.log('false')
+        console.log(arraySeparate);
+    }
+};
+
+isValid('[]()(()()')
